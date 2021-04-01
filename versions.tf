8479d7851db729b9291fb1d708a826baa9f3f9a9
Nick Gerner <hello@njgerner.com>
2021-04-01 14:40:08 -0400

    Update versions.tf

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = "~> 0.13.6"
 
   required_providers {
-    aws      = ">= 2.0.*"
+    aws      = "~> 3.0"
     template = "~> 2.0"
     local    = "~> 1.2"
     random   = "~> 2.1"
